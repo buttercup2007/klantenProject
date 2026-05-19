99edce1f60f93a52270a60393e8bf77c115703cf
buttercup2007 <2215907@talnet.nl>
2026-05-19 13:46:29 +0200

    scan and result blade

--- klantenProject/routes/web.php
+++ klantenProject/routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
+    Route::get('/scan', function () {
+        return view('scan');
+    });
+    
+    Route::get('/result', function () {
+        return view('result');
+    });
 });

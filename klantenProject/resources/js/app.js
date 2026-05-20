97e8e53d88ccbc5b8d6d0683b95357a54bb53e0a
buttercup2007 <2215907@talnet.nl>
2026-05-20 13:24:27 +0200

    laravel + vue
    working in the home blade page

--- klantenProject/resources/js/app.js
+++ klantenProject/resources/js/app.js
@@ -1,12 +1,8 @@
-import './bootstrap'
 import { createApp } from 'vue'
 import QrCode from './components/qrCode.vue'
 
-const app = createApp({
-    template: `<qr-code />`
-})
+const app = createApp({})
 
 app.component('qr-code', QrCode)
 
 app.mount('#app')
-
---
+++ klantenProject/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <h1>Laravel is working</h1>
+</body>
+</html>

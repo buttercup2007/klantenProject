b1de9f6eda6bc1c37c9fd20e75fd8c856a7b2583
buttercup2007 <2215907@talnet.nl>
2026-05-21 14:04:56 +0200

    added qr code

--- klantenProject/resources/js/components/App.vue
+++ klantenProject/resources/js/components/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Scan om de student financering quiz te maken</h1>
 
-    <router-link to="/quiz">
-      Go to quiz
-    </router-link>
-  </div>
+    <img v-if="qrCodeUrl" :src="qrCodeUrl" />
+    </div>
+
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import QRCode from 'qrcode'
+
+const qrCodeUrl = ref('')
+
+onMounted(async () => {
+  const quizUrl = `${window.location.origin}/quiz`
+
+  qrCodeUrl.value = await QRCode.toDataURL(quizUrl)
+
+})
+
+</script>
+
--- klantenProject/resources/views/quiz.blade.php
+++ klantenProject/resources/views/quiz.blade.php
@@ -4,7 +4,7 @@
     <title>Test</title>
     @vite(['resources/js/app.js', 'resources/js/quiz.vue'])
 </head>
-<body>
+<body>      
     <div id="app"></div>
 </body>
 </html>

fbff6b6a28d850251ccdd5cef31efe15f077cb83
buttercup2007 <2215907@talnet.nl>
2026-05-27 11:10:36 +0200

    cool color box that will be the background

--- klantenProject/resources/js/components/App.vue
+++ klantenProject/resources/js/components/App.vue
@@ -1,56 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const x = ref(0)
+const qrCodeUrl = ref('') 
+
+function onMousemove(e) {
+  x.value = e.clientX
+}
+</script>
+
 <template>
   <div>
-
     <h1>Scan om de student financiering quiz te maken</h1>
-
-    <div class="qrCode">
-      <img v-if="qrCodeUrl" :src="qrCodeUrl" />
-    </div>
 
     <div class="link">
       <router-link to="/quiz">
         Of klik hier om de quiz te maken!
       </router-link>
     </div>
-
   </div>
-  </template>
 
-  <style>
-    .qrCode {
-      display: flex;
-      justify-content: center;
-      margin-top: 20px;
-    }
-    .background-color {
-      background-color: #964e4a;
-    }
+  <div
+    class="movearea"
+    @mousemove="onMousemove"
+    :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }"
+  >
+    <p>Move your mouse across this div...</p>
+    <p>x: {{ x }}</p>
+  </div>
+</template>
 
+<style>
+.movearea {
+  transition: 0.3s background-color ease;
+  padding: 40px;
+}
 
+.background-color {
+  background-color: #964e4a;
+}
 
+.link {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+</style>
 
-
-
-
-  </style>
-
-
-
-
-
-
-
-
-
-<script setup>
-import { ref, onMounted } from 'vue'
-import QRCode from 'qrcode'
-
-const qrCodeUrl = ref('')
-
-onMounted(async () => {
-  const quizUrl = `http://192.168.1.66:8000/quiz`
-
-  qrCodeUrl.value = await QRCode.toDataURL(quizUrl)
-})
-</script>

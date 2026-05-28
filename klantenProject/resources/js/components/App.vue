e3f8ac874910c4151a5774611a6a5009bba93bc5
buttercup2007 <2215907@talnet.nl>
2026-05-28 10:23:45 +0200

    links

--- klantenProject/resources/js/components/App.vue
+++ klantenProject/resources/js/components/App.vue
@@ -1,51 +1,42 @@
 <script setup>
-import { ref } from 'vue'
 
-const mouseX = ref(0)
-const mouseY = ref(0)
-
-function onMousemove(e) {
-  mouseX.value = e.clientX
-  mouseY.value = e.clientY
-}
 </script>
 
 <template>
-  <div
-    class="page"
-    @mousemove="onMousemove"
-    :style="{
-  background: `
-    radial-gradient(
-      circle at ${mouseX}px ${mouseY}px,
-      
-      #0a3d91 1%,
-      #001b44 10%
-    )
-  `
-}"
-  >
+
     <h1> student financiering</h1>
 
-    <div class="link">
-      <router-link to="/quiz">
-        klik hier om de quiz te maken!
+    <div class="links">
+     <!--dropdown started stoppen en overstappen-->
+      <router-link to="/info">
+        Info
       </router-link>
+
+      <router-link to="/hbo">
+        HBO
+      </router-link>
+
+      <router-link to="/opleiding">
+        opleiding
+      </router-link>
+
+      <router-link to="/home">
+        Home
+      </router-link>
+
     </div>
-  </div>
+    
+    <div class="navigators">
+
+     <h1>INFO</h1>
+     <h1>HBO</h1>
+     <h1>OPLEIDING</h1>
+     <h1>LENING EN SCHULDEN</h1>
+
+    </div>
+
 </template>
 
 <style>
-.page {
-  width: 100%;
-  min-height: 100vh;
-  transition: background 0.05s;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  
-}
 </style>
